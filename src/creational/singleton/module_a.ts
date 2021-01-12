435b2b7628a504098383a305e4b1d64d7de34014
Lucas Wanderley <lucaswanderley4@gmail.com>
2021-01-11 16:46:05 -0800

    refactor: export module to compare

--- src/creational/singleton/module_a.ts
+++ src/creational/singleton/module_a.ts
@@ -5,3 +5,5 @@ myDatabaseClassic.add({ name: 'Lucas', age: 25 });
 myDatabaseClassic.add({ name: 'Laura', age: 22 });
 myDatabaseClassic.add({ name: 'Matheus', age: 12 });
 myDatabaseClassic.add({ name: 'Maria', age: 44 });
+
+export { myDatabaseClassic };
--- src/creational/singleton/module_b.ts
+++ src/creational/singleton/module_b.ts
@@ -1,5 +1,5 @@
 import { MyDatabaseClassic } from './db/mt-database-classic';
-import './module_a';
+import { myDatabaseClassic as MyDatabaseClassicFromModuleA } from './module_a';
 
 const myDatabaseClassic = MyDatabaseClassic.getInstance();
 myDatabaseClassic.add({ name: 'Roberto', age: 25 });
@@ -7,3 +7,5 @@ myDatabaseClassic.add({ name: 'Patricia', age: 22 });
 myDatabaseClassic.add({ name: 'Luiza', age: 12 });
 myDatabaseClassic.add({ name: 'Silvana', age: 44 });
 myDatabaseClassic.show();
+
+console.log(myDatabaseClassic === MyDatabaseClassicFromModuleA);
